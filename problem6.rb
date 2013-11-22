60d167b4dd72eca6207cd370d5dbd9cb073aca6c
Davida Marion <davidamarion@gmail.com>
2013-11-21 21:46:27 -0800

    made problem 6 even neater

--- problem6.rb
+++ problem6.rb
@@ -8,8 +8,6 @@
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 sum = 0
-1.upto(100) do |num|
-  sum += num ** 2
-end
+1.upto(100) {|num| sum += num ** 2 }
 
 (1.upto(100).inject(:+))**2 - sum
